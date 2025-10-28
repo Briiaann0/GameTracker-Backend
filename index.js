c22c0c9d50a077745de0e7871a9232dcf7f00260
Brian Ojeda <briannnnojeda@gmail.com>
2025-10-28 00:31:40 -0300

    Instalación de dependencias básicas =  Express, Mongoose, dotenv y estructura del servidor

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+require('dotenv').config(); 
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; 
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('GameTracker  funcionando listo para conectar a monngooDB y crear modelos.');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidorr corriendo en el puerto ${PORT}`);
+});
