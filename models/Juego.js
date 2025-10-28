44630a798880c50ab6d3b2bd0893f54aed3375d8
Brian Ojeda <briannnnojeda@gmail.com>
2025-10-28 01:37:42 -0300

    Implementamos Juego y Reseña a la base de datos, siguiendo la estructura del desafío.

---
+++ models/Juego.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose');
+
+
+const JuegoSchema = mongoose.Schema({
+    titulo: { 
+        type: String,
+        required: true,
+        trim: true
+    },
+    genero: { type: String, trim: true },
+    plataforma: { type: String, trim: true },
+    añoLanzamiento: { type: Number }, 
+    desarrollador: { type: String }, 
+    imagenPortada: { type: String, default:  '' },
+    descripcion: { type: String },
+    completado: { type: Boolean, default: false }, 
+    fechaCreacion: { type: Date, default: Date.now() }
+});
+
+module.exports = mongoose.model('Juego', JuegoSchema);
---
+++ models/reseña.js
@@ -0,0 +1,25 @@
+
+
+const mongoose = require('mongoose');
+
+const ResenaSchema = mongoose.Schema({
+    juegoid: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Juego', 
+        required: true
+    },
+    puntuacion: { 
+        type: Number,
+        min: 1,
+        max: 5, 
+        required: true
+    },
+    textoResena: { type: String, required: true },
+    horasJugadas: { type: Number, default: 0 },
+    dificultad: { type: String }, 
+    recomendar: { type: Boolean, default: true },
+    fechaCreacion: { type: Date, default: Date.now() },
+    fechaActualizacion: { type: Date, default: Date.now() }
+});
+
+module.exports = mongoose.model('Resena', ResenaSchema);
