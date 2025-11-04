502670c61de04bcec96c4c060a641245de0ed2ed
Brian Ojeda <briannnnojeda@gmail.com>
2025-11-03 23:52:28 -0300

    Implementamos la lógica POST  para agregar un juego a la colección.

--- controllers/juegosController.js
+++ controllers/juegosController.js
@@ -1,30 +1,45 @@
+
+const Juego = require('../models/Juego'); 
+
 exports.crearJuego = async (req, res) => {
-    
-    console.log(req.body);
-    res.json({ msg: 'Creando juego' });
+
+    try {
+       
+        const juego = new Juego(req.body); 
+
+        
+         await juego.save();
+
+       
+    res.status(201).json(juego);
+
+    } catch (error) {
+
+        console.error(error);
+        
+
+        res.status(500).send('Hubo un error al crear el juego');
+    }
 };
 
 
 exports.obtenerJuegos = async (req, res) => {
+   
+     res.json({ msg: 'Obteniendo todos los juegos' });
+   };
 
-    res.json({ msg: 'Obteniendo todos los juegos ' });
 
+ exports.actualizarJuego = async (req, res) => {
+   
+      res.json({ msg: 'Actualizando juego' });
 };
 
 
-exports.actualizarJuego = async (req, res) => {
-
-    res.json({ msg: 'Actualizando juego' });
-};
-
-
-
-exports.eliminarJuego = async (req, res) => {
+     exports.eliminarJuego = async (req, res) => {
+   
     res.json({ msg: 'Eliminando juego' });
-};
-
-
-
+   };
 exports.obtenerJuego = async (req, res) => {
+   
     res.json({ msg: 'Obteniendo juego específico' });
 };
